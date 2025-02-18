987a156d290fdb7f95ffdb83ce2265fe086abdaa
nkorra <naveen6040@gmail.com>
2025-02-18 15:54:21 +1100

    Updated website with multiple pages

--- app.py
+++ app.py
@@ -4,15 +4,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html')  # Homepage
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html')  # About Us Page
 
 @app.route('/services')
 def services():
-    return render_template('services.html')
+    return render_template('services.html')  # Services Page
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')  # Gallery Page
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')  # Contact Us Page
 
 if __name__ == '__main__':
     app.run(debug=True)
